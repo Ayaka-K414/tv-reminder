60eafea44add7d1d5015270b7e06ed08d0e64911
ayakakobayashi <414ayaka@gmail.com>
2025-02-22 22:17:14 +0900

    APIからデータ取得するまで完了

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import './App.css'
-
+import "./App.css";
+import { FetchPrograms } from "./components/FetchPrograms";
+// import { ProgramList } from "./components/ProgramList";
 function App() {
-
   return (
     <>
-    
+      {/* <ProgramList /> */}
+      <FetchPrograms />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
