6511f48f93a7a04a2c3e14e0f5e37200a2c80278
ayakakobayashi <414ayaka@gmail.com>
2025-02-22 22:26:59 +0900

    コンフリクト解消

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import "./App.css";
 import { FetchPrograms } from "./components/FetchPrograms";
-// import { ProgramList } from "./components/ProgramList";
 function App() {
   return (
     <>
-      {/* <ProgramList /> */}
       <FetchPrograms />
     </>
   );
